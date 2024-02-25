0715b12d2cd997069ffff3cd8b7c7fa1962815db
Németh Andris <n.andris939@gmail.com>
2024-02-25 18:09:26 +0100

    Add cors to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,12 +9,15 @@
 */
 
 const express = require("express");
+const cors = require('cors');
 const userRoutes = require("./routes/userRoutes");
 const getDbConnection = require("./db");
 const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/users", userRoutes);
 
 getDbConnection().then(() => {
